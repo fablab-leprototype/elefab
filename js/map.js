588d41f2e5e35bdc0ec816fc3180872a3ae6c164
Yuraï Slovaque <assistance@simplix.fr>
2018-10-27 17:12:36 +0200

    mise à jour
    gps, latitude, longitude

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 $(function() {
     // leaflet contact map init
-    var locmap = L.map('map', { scrollWheelZoom: false }).setView([ 45.5662085,5.9267079 ], 16);
+    var locmap = L.map('map', { scrollWheelZoom: false }).setView([ 45.5664111,5.9243184 ], 16);
     L.tileLayer.provider('Stamen.TonerLite').addTo(locmap);
-    L.marker([ 45.5662085,5.9267079 ]).addTo(locmap);
+    L.marker([ 45.5664111,5.9243184 ]).addTo(locmap);
 });
 
